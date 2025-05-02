df89cf14293c790f55badba2fd1c377b191c9da1
anurag654321 <2k22.it.32861@gmail.com>
2025-05-02 22:29:27 +0530

    adding atlass

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose')
 const plm=require('passport-local-mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/pinterestapp")
+mongoose.connect("mongodb+srv://2k22it32861:oVim89IJg7vrhirJ@pinterestwebapp.ww3j2va.mongodb.net/")
 
 
 const userSchema = mongoose.Schema({
